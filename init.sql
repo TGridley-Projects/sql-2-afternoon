e4bbb981eeb2af8f8eb9ba5f85bb0eec8757958f
Trevor <alutiiq@comcast.net>
2020-08-12 16:55:38 -0700

    joins and nested complete

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
--- Practice joins
+-- PRACTICE JOINS
 -- #1
 SELECT *
 FROM invoice
@@ -21,31 +21,73 @@ FROM artist
     JOIN album ON artist.artist_id = album.artist_id;
 
 -- #5 
-SELECT track_id FROM playlist_track pt
-JOIN playlist p ON pt.playlist_id = p.playlist_id
+SELECT track_id
+FROM playlist_track pt
+    JOIN playlist p ON pt.playlist_id = p.playlist_id
 WHERE p.name = 'Music';
 
 -- #6
-SELECT t.name FROM track t
-JOIN playlist_track pt ON pt.track_id = t.track_id
+SELECT t.name
+FROM track t
+    JOIN playlist_track pt ON pt.track_id = t.track_id
 WHERE pt.playlist_id = 5;
 
 --#7
-SELECT t.name, p.name FROM track t
-JOIN playlist_track pt ON pt.track_id = t.track_id
-JOIN playlist p ON p.playlist_id = pt.playlist_id;
+SELECT t.name, p.name
+FROM track t
+    JOIN playlist_track pt ON pt.track_id = t.track_id
+    JOIN playlist p ON p.playlist_id = pt.playlist_id;
 
 -- #8
-SELECT t.name, a.title FROM track t
-JOIN album a ON a.album_id = t.album_id
-JOIN genre g ON g.genre_id = t.genre_id
+SELECT t.name, a.title
+FROM track t
+    JOIN album a ON a.album_id = t.album_id
+    JOIN genre g ON g.genre_id = t.genre_id
 WHERE g.name = 'Alternative & Punk';
 
 --Black DIamond
-SELECT t.name, g.name, al.title, ar.name FROM track t
-JOIN genre g ON g.genre_id = t.genre_id
-JOIN album al ON al.album_id = t.album_id
-JOIN artist ar ON ar.artist_id = al.artist_id
-JOIN playlist_track pt ON pt.track_id = t.track_id
-JOIN playlist p ON p.playlist_id = pt.playlist_id
+SELECT t.name, g.name, al.title, ar.name
+FROM track t
+    JOIN genre g ON g.genre_id = t.genre_id
+    JOIN album al ON al.album_id = t.album_id
+    JOIN artist ar ON ar.artist_id = al.artist_id
+    JOIN playlist_track pt ON pt.track_id = t.track_id
+    JOIN playlist p ON p.playlist_id = pt.playlist_id
 WHERE p.name = 'Music';
+
+-- PRACTICE NESTED QUERIES
+-- #1
+SELECT *
+FROM invoice
+WHERE invoice_id IN(SELECT invoice_id
+FROM invoice_line
+WHERE unit_price > .99);
+
+-- #2
+SELECT * FROM playlist_track
+WHERE playlist_id IN (SELECT playlist_id FROM playlist WHERE name = 'Music');
+
+-- #3
+SELECT name from track
+WHERE track_id IN (SELECT track_id FROM playlist_track WHERE playlist_id = 5);
+
+-- #4
+SELECT * FROM track
+WHERE genre_id IN (SELECT genre_id FROM genre WHERE name = 'Comedy');
+
+-- #5
+SELECT * FROM track
+WHERE album_id IN ( SELECT album_id FROM album WHERE title = 'Fireball' );
+
+-- #6
+SELECT * FROM track
+WHERE album_id IN ( SELECT album_id from album WHERE artist_id IN(
+  SELECT artist_id FROM artist WHERE name = 'Queen'));
+
+-- PRACTICE UPDATING ROWS
+-- #1
+UPDATE customer
+SET fax = null
+WHERE fax IS NOT null;
+
+-- #2
