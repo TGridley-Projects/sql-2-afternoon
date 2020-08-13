d672d28176e1fa28610e0421bfa67f78a56cf6f8
Trevor <alutiiq@comcast.net>
2020-08-12 23:11:20 -0700

    Done through Use Distinct

--- init.sql
+++ init.sql
@@ -91,3 +91,57 @@ SET fax = null
 WHERE fax IS NOT null;
 
 -- #2
+UPDATE customer
+SET company = 'Self'
+WHERE company IS null;
+
+-- #3
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE first_name = 'Julia' AND last_name = 'Barnett';
+
+-- #4
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl';
+
+-- #5
+UPDATE track
+SET composer = 'The Darkness Around Us'
+WHERE genre_id = (SELECT genre_id FROM genre WHERE name = 'Metal')
+AND composer IS null;
+
+-- GROUP BY
+-- #1
+SELECT count(*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+GROUP BY g.name;
+
+-- #2
+SELECT count(*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+WHERE g.name IN ('Pop', 'Rock')
+GROUP BY g.name;
+
+-- #3
+SELECT a.name, COUNT(*)
+FROM album al
+JOIN artist a ON a.artist_id = al.artist_id
+GROUP BY a.name;
+
+-- USE DISTINCT
+-- #1
+SELECT DISTINCT composer
+FROM track;
+
+-- #2
+SELECT DISTINCT billing_postal_code
+FROM invoice;
+
+-- #3
+SELECT DISTINCT company
+FROM customer;
+
+-- DELETE ROWS
